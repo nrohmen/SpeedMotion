8c686c98b40ec7fb402f5cd163942f2b8618e210
Nur Rohman <ennurr31@gmail.com>
2017-11-10 22:21:06 +0700

    adding some info

--- app/src/main/java/com/sdw/speedmotion/MainActivity.java
+++ app/src/main/java/com/sdw/speedmotion/MainActivity.java
@@ -3,6 +3,8 @@ package com.sdw.speedmotion;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.github.anastr.speedviewlib.base.Gauge;
 import com.samsung.android.sdk.SsdkUnsupportedException;
@@ -13,12 +15,20 @@ public class MainActivity extends AppCompatActivity {
     private Smotion mMotion;
     private SmotionPedometer mPedometer;
     private Gauge speedView;
+    private TextView textCalorie;
+    private TextView textDistance;
+    private TextView textCount;
+    private TextView textStatus;
 
     final SmotionPedometer.ChangeListener changeListener =
             new SmotionPedometer.ChangeListener() {
                 @Override
                 public void onChanged(SmotionPedometer.Info info) {
-                    double speed = info.getSpeed();
+                    double speed = info.getSpeed() * 1.60934;
+                    textCalorie.setText(String.valueOf(info.getCalorie()));
+                    textDistance.setText(String.valueOf(info.getDistance()));
+                    textCount.setText(String.valueOf(info.getCount(SmotionPedometer.Info.COUNT_TOTAL)));
+                    textStatus.setText(getStatus(info.getStatus()));
                     speedView.speedTo((float) speed);
                 }
             };
@@ -27,7 +37,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        speedView = (Gauge) findViewById(R.id.speedometer);
+        speedView = findViewById(R.id.speedometer);
+        textCalorie = findViewById(R.id.calorie);
+        textCount = findViewById(R.id.step_count);
+        textStatus = findViewById(R.id.status);
+        textDistance = findViewById(R.id.distance);
+
         mMotion = new Smotion();
         try {
             mMotion.initialize(this);
@@ -40,5 +55,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private String getStatus(int status) {
+        String str = null;
+        switch (status) {
+            case SmotionPedometer.Info.STATUS_WALK_UP:
+                str = "Walk Up";
+                break;
+            case SmotionPedometer.Info.STATUS_WALK_DOWN:
+                str = "Walk Down";
+                break;
+            case SmotionPedometer.Info.STATUS_WALK_FLAT:
+                str = "Walk";
+                break;
+            case SmotionPedometer.Info.STATUS_RUN_DOWN:
+                str = "Run Down";
+                break;
+            case SmotionPedometer.Info.STATUS_RUN_UP:
+                str = "Run Up";
+                break;
+            case SmotionPedometer.Info.STATUS_RUN_FLAT:
+                str = "Run";
+                break;
+            case SmotionPedometer.Info.STATUS_STOP:
+                str = "Stop";
+                break;
+            case SmotionPedometer.Info.STATUS_UNKNOWN:
+                str = "Unknown";
+                break;
+            default:
+                break;
+        }
+        return str;
+    }
 
 }
